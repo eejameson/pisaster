392f56ef5430032bcf898b5253c615ac6c583108
Emily Jameson <eejameson@gmail.com>
2023-02-08 19:06:35 -0800

    added reproducible code snipet

--- R/01_summary_stats.R
+++ R/01_summary_stats.R
@@ -14,3 +14,19 @@ head(pisaster_data)
 
 ## calculate mean counts across all years, sites, and plots
 mean_count <- mean(pisaster_data$count)
+
+
+# Reproducible code snipet 
+dput(pisaster_data[1:10,c(1, 4)])
+
+
+head(pisaster_data[1:10,c(1, 4)])
+
+data <- structure(list(2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 
+                       2019, 2019, 6L, 10L, 13L, 9L, 9L, 11L, 7L, 10L, 12L, 8L), .Dim = c(10L, 
+                                                                                          2L), .Dimnames = list(NULL, c("year", "count")))
+
+head(data)
+
+mean(data$count)
+
